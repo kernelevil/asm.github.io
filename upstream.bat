d5b43790e43ddfa839ea150c098f4d5f5b6f74ff
kernelevil <547622133@qq.com>
2024-08-23 20:12:25 +0800

    local

---
+++ upstream.bat
@@ -0,0 +1,8 @@
+@echo off
+git add .
+for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set date=%%a-%%b-%%c
+for /f "tokens=1-2 delims=:," %%a in ("%time%") do set time=%%a-%%b
+set datetime=%date% %time%
+git commit -m "my info - Date: %datetime%"
+git push
+pause
